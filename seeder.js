14b3108fb8205ad8c7b1cfd5aa7337acb593c503
desxz <muratgun545@gmail.com>
2021-10-05 20:00:27 +0300

    Bootcamp Ownership

--- seeder.js
+++ seeder.js
@@ -10,6 +10,8 @@ dotenv.config({ path: './config/config.env' });
 //Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
+
 
 
 //Connect to DB
@@ -28,12 +30,19 @@ const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
+
+
 
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (err) {
@@ -46,6 +55,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
